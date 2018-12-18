7b407fdf38d5a188f251d3c469f7809cbca5d596
Max Gonzih <gonzih@gmail.com>
2018-12-17 20:03:52 -0500

    Feck

--- backend/local_main.go
+++ backend/local_main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 )
 
 func localMain() {
-	data := readServersYaml()
-
-	for name, host := range data.Pc {
-		log.Println(name)
-		h := &Host{Name: name, HostName: host}
-		h.Scan(data.Ports)
-		up, err := h.IsUp()
-		h.IsHostUp = up
-		log.Printf("%s is %v: %v", name, up, err)
-	}
+	data, err := HandleRequest(context.TODO())
+	log.Println(err)
+	b, _ := json.Marshal(data)
+	log.Println(string(b))
 }
