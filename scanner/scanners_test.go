5beca22619823e3a6587761df8945c9bd869dd6c
Max Gonzih <gonzih@gmail.com>
2018-12-24 11:37:17 -0500

    Unifying scanner tests

--- scanner/scanners_test.go
+++ scanner/scanners_test.go
@@ -17,9 +17,10 @@ func TestScanBuiltIn(t *testing.T) {
 func TestScanNmap(t *testing.T) {
 	ports, err := NmapScan("google.com")
 	assert.Nil(t, err)
-	assert.Len(t, ports, 2)
-	assert.Equal(t, ports[0].Port, 80)
-	assert.True(t, ports[0].Open)
-	assert.Equal(t, ports[1].Port, 443)
-	assert.True(t, ports[1].Open)
+	assert.True(t, len(ports) > 1)
+	for _, port := range ports {
+		if port.Port == 80 || port.Port == 443 {
+			assert.True(t, ports[0].Open)
+		}
+	}
 }
