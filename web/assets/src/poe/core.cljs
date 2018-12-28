6d9fb21d2f7d46ff8025acfe8d0cfed27daa155c
Max Gonzih <gonzih@gmail.com>
2018-12-27 21:10:20 -0500

    Updates

--- web/assets/src/poe/core.cljs
+++ web/assets/src/poe/core.cljs
@@ -1,10 +1,27 @@
 (ns poe.core
-  (:require [reagent.core :as r]
-            [clojure.walk :refer [keywordize-keys]]))
+  (:require [clojure.walk :refer [keywordize-keys]]
+            [clojure.string :as strings]
+            [reagent.core :as r]))
 
 (enable-console-print!)
 
-(defn navbar []
+(def flags
+  {"amsterdam" "🇳🇱"
+   "australia" "🇦🇺"
+   "brazil" "🇧🇷"
+   "california" "🇺🇸"
+   "frankfurt" "🇩🇪"
+   "japan" "🇯🇵"
+   "london" "🇬🇧"
+   "milan" "🇮🇹"
+   "moscow" "🇷🇺"
+   "russia" "🇷🇺"
+   "paris" "🇫🇷"
+   "singapore" "🇸🇬"
+   "texas" "🇺🇸"
+   "washington d.c." "🇺🇸"})
+
+(defn navbar-component []
   [:nav.navbar {:role "navigation" :aria-label "main navigation"}
    [:div.container
     [:div.navbar-brand
@@ -16,26 +33,35 @@
 (defonce state (r/atom nil))
 (defonce active-tab (r/atom "PC"))
 
-(defn server-state [{:keys [up_evidence down_evidence]}]
+(defn server-status [{:keys [up_evidence down_evidence]}]
   (let [up-noe (or (:number_of_samples up_evidence) 0)
         down-noe (or (:number_of_samples down_evidence) 0)
-        up? (>= up-noe down-noe)
-        label (if up? "Up" "Down")
-        klass (if up? "fa-check" "fa-exclamation-triangle")
+        up? (>= up-noe down-noe)]
+    (if up?
+      (if (< up-noe (* down-noe 3))
+        :unstable
+        :up)
+      :down)))
+
+(defn server-state-component [{:keys [up_evidence down_evidence] :as server}]
+  (let [up-noe (or (:number_of_samples up_evidence) 0)
+        down-noe (or (:number_of_samples down_evidence) 0)
+        status (server-status server)
         title (str up-noe " positive reports, " down-noe " negative reports")]
     [:td
-     (if up?
-       (if (< up-noe (* down-noe 3))
-         [:span.tag.is-warning {:title title} "Server is unstable"]
-         [:span.tag.is-success {:title title} "Server is up"])
-       [:span.tag.is-danger {:title title} "Server is down"])]))
+     (case status
+       :up       [:span.tag.is-success {:title title} "Server is up"]
+       :down     [:span.tag.is-danger {:title title} "Server is down"]
+       :unstable [:span.tag.is-warning {:title title} "Server is unstable"])]))
 
 (defn server-component [{:keys [server_name platform] :as server}]
   [:tr
-   [:td server_name]
-   [server-state server]])
+   [:td
+    [:span.flag (flags (strings/lower-case server_name))]
+    server_name]
+   [server-state-component server]])
 
-(defn server-table []
+(defn server-table-component []
   [:table.table.is-stripped
    [:tbody
     (let [data (filter #(= (:platform %) @active-tab)
@@ -46,20 +72,72 @@
                       (:platform server))}
           [server-component server])))]])
 
+(defn servers-component []
+  [:div.container
+   [:div.tabs
+    [:ul
+     (doall
+       (for [fil ["PC" "XBOX"]]
+         ^{:key fil}
+         [:li {:class (when (= @active-tab fil) "is-active")}
+          [:a {:on-click #(reset! active-tab fil)} "Login for " fil]]))]]
+   [server-table-component]])
+
+(defn content-component []
+  [:div.content
+   [:h2 "What is this site?"]
+   [:hr]
+   [:p
+    "We continuously monitor the status of "
+    [:a {:href "https://www.pathofexile.com/" :target "_blank"} "Path of Exile"]
+    " login servers. If there are any interruptions in service it should be indicated on this page."]])
+
+(defn notification-component []
+  (let [len (count @state)
+        statuses (map server-status @state)
+        num-down (count (filter #(= :down %) statuses))
+        num-unstable (count (filter #(= :unstable %) statuses))
+        overall-status (if (zero? num-down)
+                          (if (zero? num-unstable)
+                            :up
+                            :unstable)
+                          :down)
+        klasses {:up "is-success" :down "is-danger" :unstable "is-warning"}
+        msgs {:up "All servers are operational" :down "Some servers might be down" :unstable "Some servers might be unstable"}
+        icons {:up "fa-check-circle" :down "fa-times-circle" :unstable "fa-exclamation-circle"}]
+    [:div.notification {:class (overall-status klasses)}
+     [:i.fas {:class (overall-status icons)}]
+     (overall-status msgs)]))
+
+(defn footer-component []
+  [:footer.footer
+   [:div.content.has-text-centered
+    [:span
+     [:a {:href "http://poe-status.com" }"poe-status.com"]
+     " by "
+     [:a {:href "https://github.com/Gonzih" :target "_blank"} "Max Gonzih"]]]
+   [:div.content.has-text-centered.is-small
+    [:span
+     "Powered by "
+     [:a {:href "https://golang.org/" :target "_blank"} "Go"]
+     ", "
+     [:a {:href "https://clojurescript.org/" :target "_blank"} "ClojureScript"]
+     ", "
+     [:a {:href "https://bulma.io/" :target "_blank"} "Bulma"]
+     "."]
+    [:br]
+    [:span "This site is fan-made and not affiliated with Grinding Gear Games in any way."]]])
+
 (defn root-component []
   [:div
-   [navbar]
-   [:div.section
-    [:div.container
-     [:div.column
-      [:div.tabs
-       [:ul
-        (doall
-          (for [fil ["PC" "XBOX"]]
-            ^{:key fil}
-            [:li {:class (when (= @active-tab fil) "is-active")}
-             [:a {:on-click #(reset! active-tab fil)} fil]]))]]
-      [server-table]]]]])
+   [:div.colums
+    [:div.column
+     [:div.section
+      ; [navbar-component]
+      [notification-component]
+      [content-component]
+      [servers-component]]]]
+   [footer-component]])
 
 (defn init! []
   (r/render [root-component]
